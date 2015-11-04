5cb3c64336b718e3982cf5d5a5c23667abe97db7
Yang Bo <yang.bo@gses.com>
2015-11-04 16:13:01 -0500

    display sun rise time for eldersburg

--- web/js/main.js
+++ web/js/main.js
@@ -29,8 +29,31 @@ function button3Clicked(){
     $("#response").empty();
 }
 
+function button4Clicked(){
+    getSolarInfo();
+}
+
+function getSolarInfo(){
+    $.ajax({
+        url: "http://api.sunrise-sunset.org/json?lat=39.40083333&lng=-76.95138889&date=today&formatted=0",
+        type:"GET",
+        success: function(response){
+            if(response.status == "OK"){
+                var sunrise = new Date(response.results.sunrise).toLocaleTimeString();
+                var sunset = new Date(response.results.sunset).toLocaleTimeString();
+                var noon =new Date(response.results.solar_noon).toLocaleTimeString();
+                $("#response").html("Today sun rise: " + sunrise +", sun set: "+ sunset + ", noon: "+noon);
+            }
+        },
+        error:function(e){
+            console.log(e);
+        }
+    });
+}
+
 $( document ).ready(function() {
     $("#button1").on("click",button1Clicked);
     $("#button2").on("click",button2Clicked);
     $("#button3").on("click",button3Clicked);
+    $("#button4").on("click",button4Clicked);
 });
